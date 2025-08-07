2dfb8e1736efd6d57ea80a2365f6e4c1d5612ac4
robertinws <bertinws@gmail.com>
2025-08-07 09:06:38 -0300

    Controller criado

---
+++ lib/app_controller.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pr_module_c_project/pages/login_page.dart';
+import 'package:pr_module_c_project/pages/splash_page.dart';
+
+class AppController extends StatelessWidget {
+  const AppController({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashPage(),
+        '/login': (context) => LoginPage(),
+      },
+    );
+  }
+}
